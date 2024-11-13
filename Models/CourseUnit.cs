0a581b558d2376091a5e102cf8ea924c4e25f0d1
GAKURU <GAKURU@FEI5CG8284XK5>
2024-11-13 09:52:25 -1200

    Add project files.

---
+++ Models/CourseUnit.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentWebApp.Models
+{
+    public class CourseUnit
+    {
+        [Key]
+        public int Id { get; set; }
+        public string CourseUnitCode {  get; set; }
+        public string name {  get; set; }
+        public string DoneInYear { get; set; } 
+    }
+}
---
+++ Models/StudentDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics.Internal;
+
+namespace StudentWebApp.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public StudentDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<CourseUnit> CourseUnits { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = _configuration.GetConnectionString(name: "DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+
+    }
+}
